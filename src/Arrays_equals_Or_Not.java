cd2cef2c4550cdc6bc9b96c3ac6451615d68bef7
Arvetisatish <arvetisatishbabu@gmail.com>
2024-08-20 10:36:22 +0530

    commit 1

--- src/Arrays_equals_Or_Not.java
+++ src/Arrays_equals_Or_Not.java
@@ -1,4 +1,4 @@
-public class Arrays_equal_Or_Not {
+public class Arrays_equals_Or_Not {
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,5};
         int [] arr2= {1,2,3,4,5};
